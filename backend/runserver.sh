dabd6f9e4dc3858e3ba721ab11d9dab5bc2b0e54
Ahmed Fatir <afatir@e2r10p12.1337.ma>
2024-10-25 00:53:59 +0100

    update

--- backend/runserver.sh
+++ backend/runserver.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+cat > /root/.bashrc <<EOL
+alias bashup='source ~/.bashrc'
+alias ..='cd ..'
+alias rmf='rm -rf'
+alias py='python3.9'
+alias pym='python3.9 manage.py'
+alias pymm='python3.9 manage.py makemigrations && python3.9 manage.py migrate'
+alias pymr='python3.9 manage.py runserver'
+alias pymt='python3.9 manage.py test --verbosity 2'
+alias pyma='python3.9 manage.py createsuperuser --username admin --email admin@email.com'
+alias pipi='pip install -r requirements.txt'
+alias pipf='pip freeze > requirements.txt'
+EOL
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
